cf89fb5f8ed63a30e0faf9efc8fb8186d389d606
Seda Gezginci <gezginciseda@gmail.com>
2023-05-23 14:35:48 +0300

    first commit

---
+++ loops/EvenNumberAndMultipleOf4/src/EvenNumberAndMultipleOf4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvenNumberAndMultipleOf4 {
+    public static void main(String[] args) {
+        int n;
+        int total=0;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter number : ");
+            n = scan.nextInt();
+            if (n % 2 == 0 && n % 4 == 0) {
+                total += n;
+            }
+        } while (n % 2 != 1);
+
+        System.out.println("Total : " + total);
+
+    }
+}
