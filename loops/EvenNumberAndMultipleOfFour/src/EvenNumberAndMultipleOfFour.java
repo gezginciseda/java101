a2a8f2465d65132ee334a684f588d788543a92d3
Seda Gezginci <gezginciseda@gmail.com>
2023-05-23 14:39:19 +0300

    first commit

--- loops/EvenNumberAndMultipleOfFour/src/EvenNumberAndMultipleOfFour.java
+++ loops/EvenNumberAndMultipleOfFour/src/EvenNumberAndMultipleOfFour.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvenNumberAndMultipleOf4 {
+public class EvenNumberAndMultipleOfFour {
     public static void main(String[] args) {
         int n;
         int total=0;
